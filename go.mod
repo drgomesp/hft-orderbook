c1dd93f6b4b54c41df736406a17b27e6e9d1bd94
Daniel Ribeiro <drgomesp@gmail.com>
2024-11-17 21:13:34 -0300

    added deps and sample main code

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module go-hft-orderbook
 
 go 1.22
+
+require (
+	github.com/emirpasic/gods v1.18.1
+	github.com/shopspring/decimal v1.4.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "log"
+import (
+	"log"
+	"github.com/shopspring/decimal"
+)
 
 func main() {
+	o1 := NewOrder("sell-1", Sell, decimal.NewFromInt(10), decimal.NewFromFloat(99.56))
+	o2 := NewOrder("sell-2", Sell, decimal.NewFromInt(10), decimal.NewFromFloat(99.58))
+	o3 := NewOrder("sell-3", Sell, decimal.NewFromInt(10), decimal.NewFromFloat(99.60))
+	o4 := NewOrder("sell-4", Sell, decimal.NewFromInt(10), decimal.NewFromFloat(99.62))
+	o5 := NewOrder("sell-5", Sell, decimal.NewFromInt(10), decimal.NewFromFloat(99.64))
+
 	log.Println("hello world")
 }
